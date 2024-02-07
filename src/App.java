b11fd15d84f32e52c8127c647ec73415a9c1c62d
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2024-02-07 09:56:59 +0100

    en runda

--- src/App.java
+++ src/App.java
@@ -7,13 +7,65 @@ public class App {
 
         Scanner kb = new Scanner(System.in);
 
-        System.out.println("Starting game");
-        System.out.println("You start with 20 points in your score");
-        System.out.println("Generating random number between 1 and 8");
-        
-        Random num = new Random();
-        int rando = num.nextInt(9);
-        System.out.println("Random number:" +rando);
+        int score = 20;
 
-    }
+        System.out.println("Starting game");
+        kb.nextLine();
+        System.out.println("You start with 20 points in your score");
+        kb.nextLine();
+        System.out.println("Generating random number between 0 and 8");
+        kb.nextLine();
+        
+        Random rando = new Random();
+        int  num1 = rando.nextInt(9);
+        System.out.println("Random number:" +num1);
+
+        Random rando2 = new Random();
+                int num2 = rando2.nextInt(9);
+
+
+while (score>0) {
+    
+
+        switch (num1) {
+            case 0:
+                System.out.println("You Lost");
+                score--;
+                kb.nextLine();
+                System.out.println("You have "+score+" points");
+                break;
+        
+            default:
+                System.out.println("Generating random number for bot");
+                kb.nextLine();
+
+                System.out.println("Random number:" +num2);
+                break; 
+            }
+
+                int diff = num1-num2;
+
+                if (diff<0) {
+                    System.out.println("You Lost");
+                    score--;
+                    kb.nextLine();
+                    System.out.println("You hav"+score+"points");
+                }
+                else{
+                    System.out.println("You WIN");
+                    score++;
+                    kb.nextLine();
+                    System.out.println("You have"+score+"points");
+                        break;
+                    }
+                    
+                    
+            
+        }
+
+
+
+    } 
 }
+
+
